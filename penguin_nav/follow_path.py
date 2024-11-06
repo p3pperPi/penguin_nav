d8ebdd87a065539aea7dc837589c89f8aedc7a44
nonanonno <as.nonanonno@gmail.com>
2024-11-06 15:29:14 +0000

    feat: support multiple waypoint files

--- penguin_nav/follow_path.py
+++ penguin_nav/follow_path.py
@@ -11,15 +11,18 @@ import math
 import sys
 
 # Columns configuration
-X="x"
-Y="y"
+# X = "x"
+# Y = "y"
+# for pcl_pose
+X = "/pcl_pose/pose/pose/position/x"
+Y = "/pcl_pose/pose/pose/position/y"
+
 
 class Controller:
     def __init__(self):
         self.navigator = BasicNavigator()
 
-
-    def to_pose(self, x,y, yaw):
+    def to_pose(self, x, y, yaw):
         q = Rotation.from_euler("z", yaw).as_quat()
 
         pose = PoseStamped()
@@ -32,7 +35,6 @@ class Controller:
         pose.pose.orientation.y = q[1]
         pose.pose.orientation.z = q[2]
         pose.pose.orientation.w = q[3]
-        print(x,y,yaw)
         return pose
 
     def go_through(self, poses):
@@ -42,27 +44,26 @@ class Controller:
         while not self.navigator.isTaskComplete():
             i += 1
             if (feedback := self.navigator.getFeedback()) and i % 5 == 0:
-                print(f"Estimated time of arrival: {Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9:.2f} seconds")
+                self.navigator.debug(
+                    f"Estimated time of arrival: {Duration.from_msg(feedback.estimated_time_remaining).nanoseconds / 1e9:.2f} seconds"
+                )
 
         result = self.navigator.getResult()
         if result == TaskResult.SUCCEEDED:
-            print("Navigation succeeded")
+            self.navigator.info("Navigation succeeded")
         elif result == TaskResult.CANCELED:
-            print("Navigation canceled")
+            self.navigator.info("Navigation canceled")
         elif result == TaskResult.FAILED:
-            print("Navigation failed")
+            self.navigator.info("Navigation failed")
         else:
-            print("Navigation unknown result")
+            self.navigator.info("Navigation unknown result")
+
+    def stop(self):
+        self.navigator.info("Stopping navigation")
+        self.navigator.cancelTask()
 
 
-def main():
-    rclpy.init()
-
-    controller = Controller()
-
-
-    df = pd.read_csv(sys.argv[1])
-
+def df_to_poses(df: pd.DataFrame):
     poses = []
 
     for i in range(len(df)):
@@ -70,17 +71,39 @@ def main():
 
     for i in range(len(poses) - 1):
         x1, y1, _ = poses[i]
-        x2, y2, _ = poses[i+1]
+        x2, y2, _ = poses[i + 1]
         yaw = math.atan2(y2 - y1, x2 - x1)
         poses[i] = (x1, y1, yaw)
 
     # repeat tail yaw
     poses[-1] = (poses[-1][0], poses[-1][1], poses[-2][2])
+    return poses
 
-    controller.go_through(poses)
 
-    # go back to initial position
-    # controller.go_through([(-2,-0.5,0)])
+def main():
+    rclpy.init()
+
+    controller = Controller()
+
+    files = sys.argv[1:]
+
+    try:
+        for i, file in enumerate(files):
+            controller.navigator.get_logger().info(
+                f"Running {file} ... {i+1}/{len(files)}"
+            )
+            df = pd.read_csv(file)
+            poses = df_to_poses(df)
+            controller.go_through(poses)
+            val = input(
+                f"Finish {i+1}th file. Press enter to continue. Type 'q' to quit."
+            )
+            if val == "q":
+                break
+    except KeyboardInterrupt:
+        controller.stop()
+        raise
+
 
 if __name__ == "__main__":
     main()
